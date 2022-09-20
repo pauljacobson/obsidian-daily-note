c2b0f62dc4a1332d2b8a1171942e70c8055c6472
Paul Jacobson <pljcbsn@gmail.com>
2022-09-20 08:06:52 +0300

    Additional comments on the script

--- scripts/daily_note.py
+++ scripts/daily_note.py
@@ -6,17 +6,23 @@ from datetime import datetime, timedelta
 
 import requests
 
+# These definitions should probably
+# be moved to the main function
 dw = datetime.today()
 tomorrow = "{:%Y-%m-%d}".format(dw + timedelta(days=1))
 yesterday = "{:%Y-%m-%d}".format(dw - timedelta(days=1))
 title = "{:%A, %d %B %Y}".format(dw)
 year = "{:%Y-%m-%d}".format(dw)
 
+# Define the path to your daily notes directory
 PATH = "/path/to/your/directory/"
+# Define the format of the filename
+# This format adds the date to the filename
 filename = "{}{}.md".format(PATH, year)
 
 
 def get_weather(location):
+    """Source basic weather information from wttr.in"""
     payload = {"format": "3"}
     weather_request = requests.get("http://wttr.in/" + location, params=payload)
     return weather_request.text.strip()
